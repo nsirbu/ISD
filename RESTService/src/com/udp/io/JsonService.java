1829602c48fdc8246ac76a4890ddc5203112e6a7
Sirbu Nicolae <niku.sirbu@gmail.com>
2016-02-10 13:50:49 +0200

    Modified.

--- RESTService/src/com/udp/io/JsonService.java
+++ RESTService/src/com/udp/io/JsonService.java
@@ -1,5 +1,20 @@
 package com.udp.io;
 
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+
+import isd.model.Message;
+
 public class JsonService {
+	
+public static JsonArray createJSArray(ArrayList<Message> data){
+		
+		JsonArray jsonData = null;
+		Gson gson = new GsonBuilder().create();
+		return jsonData = gson.toJsonTree(data).getAsJsonArray();
+	}
 
 }

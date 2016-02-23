c11a5bf085b8739c7c09dfd41466d781f07915bf
Stanislav Scerbatiuc <scerbatiuc.stas@gmail.com>
2016-02-23 11:50:56 +0200

    Implemented threshold values modification

--- RESTService/src/com/udp/helper/TimeHelper.java
+++ RESTService/src/com/udp/helper/TimeHelper.java
@@ -16,7 +16,7 @@ import com.udp.io.Log4j;
  * @author sscerbatiuc
  */
 public class TimeHelper {
-	
+
 	static Logger log = Log4j.initLog4j(TimeHelper.class);
 
 	/**
@@ -27,13 +27,13 @@ public class TimeHelper {
 	public static String getCurrentTime() {
 
 		Calendar calendar = Calendar.getInstance();
-		int year 		  = calendar.get(Calendar.YEAR);
-		int month		  = calendar.get(Calendar.MONTH) + 1;
-		int day 		  = calendar.get(Calendar.DAY_OF_MONTH);
-		int hours 		  = calendar.get(Calendar.HOUR_OF_DAY);
-		int minutes 	  = calendar.get(Calendar.MINUTE);
-		int seconds		  = calendar.get(Calendar.SECOND);
-		int milliseconds  = calendar.get(Calendar.MILLISECOND);
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH) + 1;
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		int hours = calendar.get(Calendar.HOUR_OF_DAY);
+		int minutes = calendar.get(Calendar.MINUTE);
+		int seconds = calendar.get(Calendar.SECOND);
+		int milliseconds = calendar.get(Calendar.MILLISECOND);
 		String currentTimeString = year + "-" + month + "-" + day + " "
 				+ (hours < 10 ? "0" + hours : hours) + ":"
 				+ (minutes < 10 ? "0" + minutes : minutes) + ":"
@@ -41,34 +41,54 @@ public class TimeHelper {
 				+ (milliseconds);
 		return currentTimeString;
 	}
+
+	/**
+	 * Returns the current date.
+	 * 
+	 * @param pattern
+	 *            String - representing the date format. If <b>null</b> 
+	 *            "yyyy-MM-dd" pattern will be used
+	 * 
+	 * @return String <code>currentDate</code>
+	 */
+	public static String getCurrentDateString(String pattern) {
+		// "yyyy-MM-dd"
+		if(pattern == null) pattern = "yyyy-MM-dd";
+		DateFormat dateFormat = new SimpleDateFormat(pattern);
+		Calendar calendar = Calendar.getInstance();
+		String currentDate = dateFormat.format(calendar.getTime());
+		return currentDate;
+	}
 	
 	/**
-	 * Returns the current date. <b>Format: </b>"yyyy-MM-dd"
-	 * @param pattern TODO
-	 * 
-	 * @return String <code>currentTime</code>
+	 * Parses the given String and returns a <code>Date</code> object
+	 * @param date - String
+	 * @return Date object
 	 */
-	public static String getCurrentDate(String pattern) {
-
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		Calendar calendar = Calendar.getInstance();		
-		String currentDate = dateFormat.format(calendar.getTime());
-				
-		return currentDate;
+	public static Date getCurrentDate(String date){
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			Date currentDate = new Date();
+			currentDate = dateFormat.parse(date);
+			return currentDate;
+		} catch (Exception e) {
+			log.error("Couldn't parse the given date");
+			return null;
+		}
 	}
 
 	/**
-	 * Returns a textual representation of the current time.
+	 * Returns a textual representation of the current time (<code>String</code>).
 	 *
 	 * @return String currentTimeStamp - day + month + hours + minutes
 	 */
 	public static String getCurrentTimeStamp() {
 		Calendar calendar = Calendar.getInstance();
-		int hours	= calendar.get(Calendar.HOUR_OF_DAY);
+		int hours = calendar.get(Calendar.HOUR_OF_DAY);
 		int minutes = calendar.get(Calendar.MINUTE);
 		int seconds = calendar.get(Calendar.SECOND);
-		int month 	= calendar.get(Calendar.MONTH) + 1;
-		int day 	= calendar.get(Calendar.DAY_OF_YEAR);
+		int month = calendar.get(Calendar.MONTH) + 1;
+		int day = calendar.get(Calendar.DAY_OF_YEAR);
 
 		String currentTimeStamp = String.valueOf(day) + String.valueOf(month)
 				+ String.valueOf(hours) + String.valueOf(minutes)
@@ -80,53 +100,68 @@ public class TimeHelper {
 
 	/**
 	 * Returns the number of milliseconds that have passed since a given date.
-	 * @param inspectedDateString <code>String</code> - The inspected date
-	 * @return difference <code>long</code> - How much time has passed since the inspected date 
-	 * @throws ParseException if the provided date string is not an acceptable format
+	 * 
+	 * @param inspectedDateString
+	 *            <code>String</code> - The inspected date
+	 * @return difference <code>long</code> - How much time has passed since the
+	 *         inspected date
+	 * @throws ParseException
+	 *             if the provided date string is not an acceptable format
 	 */
-	public static long howMuchMillisSince(String inspectedDateString) throws ParseException{
-		
-		SimpleDateFormat sdtFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
-		Date currentTime   = sdtFormat.parse(TimeHelper.getCurrentTime());
+	public static long howMuchMillisSince(String inspectedDateString)
+			throws ParseException {
+
+		SimpleDateFormat sdtFormat = new SimpleDateFormat(
+				"yyyy-MM-dd HH:mm:ss.S");
+		Date currentTime = sdtFormat.parse(TimeHelper.getCurrentTime());
 		Date inspectedTime = sdtFormat.parse(inspectedDateString);
-		long difference	   = currentTime.getTime() - inspectedTime.getTime();
+		long difference = currentTime.getTime() - inspectedTime.getTime();
 		return difference;
-			
+
 	}
-	
+
 	/**
 	 * Returns the number of milliseconds between 2 given dates
+	 * 
 	 * @param date1
 	 * @param date2
-	 * @return difference <code>long</code> - How much time has passed between 2 given dates
-	 * @throws ParseException if the provided date string is not an acceptable format
+	 * @return difference <code>long</code> - How much time has passed between 2
+	 *         given dates
+	 * @throws ParseException
+	 *             if the provided date string is not an acceptable format
 	 */
-	public static long getDifference(String date1, String date2) throws ParseException{
-		
-		SimpleDateFormat sdtFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
-		Date currentTime   = sdtFormat.parse(date1);
+	public static long getDifference(String date1, String date2)
+			throws ParseException {
+
+		SimpleDateFormat sdtFormat = new SimpleDateFormat(
+				"yyyy-MM-dd HH:mm:ss.S");
+		Date currentTime = sdtFormat.parse(date1);
 		Date inspectedTime = sdtFormat.parse(date2);
-		long difference	   = currentTime.getTime() - inspectedTime.getTime();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(currentTime);;
+		long difference = currentTime.getTime() - inspectedTime.getTime();
 		return difference;
 	}
-	
+
 	public static String extractStringDateFromString(String dateToProcess) {
 
 		return dateToProcess.substring(0, 10);
 	}
-	
+
 	public static String extractStringHourFromString(String dateToProcess) {
-		
-		return dateToProcess.substring(dateToProcess.length() - 8, dateToProcess.length()); 
+
+		return dateToProcess.substring(dateToProcess.length() - 8,
+				dateToProcess.length());
 	}
-	
+
 	public static Date extractDateFromString(String dateToProcess) {
 		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
 		Date date = null;
 		try {
 			date = fmt.parse(dateToProcess);
 		} catch (ParseException e) {
-			log.error("Exception in extractDateFromString() function, TimeHelper class : " + e.getMessage());
+			log.error("Exception in extractDateFromString() function, TimeHelper class : "
+					+ e.getMessage());
 		}
 		return date;
 	}
